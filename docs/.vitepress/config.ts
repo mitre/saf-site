fed1f8961cdca0474acd46e0a64141004a51f3e4
Aaron Lippold <lippold@gmail.com>
2026-01-24 17:54:02 -0500

    fix: correct GitHub link to mitre/saf-site
    Authored by: Aaron Lippold<lippold@gmail.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -57,7 +57,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/mitre/saf' },
+      { icon: 'github', link: 'https://github.com/mitre/saf-site' },
     ],
 
     footer: {
